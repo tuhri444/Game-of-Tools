70c4a7148fb476717981bea968597e0a6cbf028b
Arjen de Aldrey <adealdrey3@gmail.com>
2022-06-30 14:54:47 +0200

    [FIX] aspect ratio now influences block size

--- Advanced_Tools_Arjen_de_Aldrey/Assets/Scripts/SpawnBlocks.cs
+++ Advanced_Tools_Arjen_de_Aldrey/Assets/Scripts/SpawnBlocks.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class SpawnBlocks : MonoBehaviour
 {
     [SerializeField] private GameObject blockPrefab;
-    private int screenWidth;
-    private int screenHeight;
     private Block[,] blocks;
     private List<Block> blocksList;
-    [Range(1,10)][SerializeField] private int blockSize;
+    [Range(1,10000)][SerializeField] private int blockAmountWidth;
+    [Range(1,10000)][SerializeField] private int blockAmountHeight;
 
-    private int blockWidth;
-    private int blockHeight;
-    private int blockAmountHor;
-    private int blockAmountVert;
+    private float blockWidth;
+    private float blockHeight;
 
     [SerializeField] private GameObject parentObject;
     private bool start;
@@ -27,12 +24,8 @@ public class SpawnBlocks : MonoBehaviour
 
     void Start()
     {
-        screenHeight = Screen.height;
-        screenWidth = Screen.width;
-        blockWidth = 12* blockSize;
-        blockHeight = 12* blockSize;
-        blockAmountHor = screenWidth / blockWidth;
-        blockAmountVert = screenHeight / blockHeight;
+        blockWidth = Screen.width / (float)blockAmountWidth;
+        blockHeight = Screen.height / (float)blockAmountHeight;
         blocksList = new List<Block>();
         generateMap();
     }
@@ -61,40 +54,40 @@ public class SpawnBlocks : MonoBehaviour
 
     private void generateMap()
     {
-        blocks = new Block[blockAmountHor, blockAmountVert+1];
-        for(int i = 0;i < blockAmountVert; i++)
+        blocks = new Block[blockAmountWidth, blockAmountHeight + 1];
+        for(int i = 0;i < blockAmountHeight; i++)
         {
-            for (int j = 0; j < blockAmountHor; j++)
+            for (int j = 0; j < blockAmountWidth; j++)
             {
                 Vector2 pos = new Vector2(j*blockWidth+blockWidth*0.5f, i * blockHeight + blockHeight * 0.5f);
                 blocks[j, i] = GameObject.Instantiate(blockPrefab, new Vector3(pos.x,pos.y,0),Quaternion.identity).GetComponent<Block>();
                 blocks[j, i].GetComponent<RectTransform>().sizeDelta = new Vector2(blockWidth,blockHeight);
                 blocks[j, i].transform.parent = parentObject.transform;
-                int randomnumba = Random.Range(0, 4);
+                //int randomnumba = Random.Range(0, 4);
                 //if (randomnumba == 1) blocks[j, i].alive = true;
                 blocksList.Add(blocks[j, i]);
             }
         }
-        for (int i = 0; i < blockAmountVert; i++)
+        for (int i = 0; i < blockAmountHeight; i++)
         {
-            for (int j = 0; j < blockAmountHor; j++)
+            for (int j = 0; j < blockAmountWidth; j++)
             {
                 //north
-                if (i + 1 < blockAmountVert) blocks[j, i].North = blocks[j, i + 1];
+                if (i + 1 < blockAmountHeight) blocks[j, i].North = blocks[j, i + 1];
                 //south
                 if(i - 1 >= 0) blocks[j, i].South = blocks[j, i - 1];
                 //east
-                if (j+1 < blockAmountHor) blocks[j, i].East = blocks[j+1, i];
+                if (j+1 < blockAmountWidth) blocks[j, i].East = blocks[j+1, i];
                 //west
                 if (j-1 >= 0) blocks[j, i].West = blocks[j - 1, i];
                 //north-west
-                if (i + 1 < blockAmountVert && j - 1 >= 0) blocks[j, i].NorthW = blocks[j - 1, i + 1];
+                if (i + 1 < blockAmountHeight && j - 1 >= 0) blocks[j, i].NorthW = blocks[j - 1, i + 1];
                 //north-east
-                if (i + 1 < blockAmountVert && j + 1 < blockAmountHor) blocks[j, i].NorthE = blocks[j + 1, i + 1];
+                if (i + 1 < blockAmountHeight && j + 1 < blockAmountWidth) blocks[j, i].NorthE = blocks[j + 1, i + 1];
                 //south-west
                 if (i - 1 >= 0 && j - 1 >= 0) blocks[j, i].SouthW = blocks[j - 1, i - 1];
                 //south-east
-                if (i - 1 >= 0 && j + 1 < blockAmountHor) blocks[j, i].SouthE = blocks[j + 1, i - 1];
+                if (i - 1 >= 0 && j + 1 < blockAmountWidth) blocks[j, i].SouthE = blocks[j + 1, i - 1];
             }
         }
 
